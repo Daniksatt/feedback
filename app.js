3cb8ee13757a03cb8d40f2a04d56b4a56437c16b
Satt Dani <daniisatt@gmail.com>
2018-02-03 22:42:14 +0700

    this is final

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var thanks= require('./routes/thanks');
 
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
@@ -27,6 +28,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+app.use('/thanks', thanks);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
